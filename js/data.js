e9b48190fb4494046bb30286308e20adc614234b
lihaitao <lihaitao.me@qq.com>
2023-02-27 22:32:20 +0800

    修改简历内容

--- js/data.js
+++ js/data.js
@@ -12,7 +12,7 @@ window.resume = {
 	"email" : "lihaitaomail@126.com",
 	"github" : "https://github.com/lihthub",
 	"profile_photo" : "images/lihaitao.png",
-	"position_applied" : "Java工程师",
+	"position_applied" : "前端工程师",
 	"work_city" : "南京",
 	"work_exp" :
 	[
@@ -22,7 +22,7 @@ window.resume = {
 			"company" : "鼎捷软件股份有限公司江苏分公司",
 			"position" : "Java资深工程师",
 			"city" : "南京",
-			"description" : "1. 负责鼎捷 PaaS 平台的微服务平台设计与开发工作；<br>2. 负责鼎捷云开发者门户后端设计与开发工作；<br>3. 负责鼎捷 Athena 低代码平台后端设计与开发工作"
+			"description" : "1. 鼎捷 PaaS 平台中心微服务平台开发<br>2. 鼎捷云开发者门户后端开发<br>3. 鼎捷 Athena 开发平台低代码模块后端开发"
 		},
 		{
 			"form_date" : "2019.03",
@@ -30,7 +30,7 @@ window.resume = {
 			"company" : "江苏敏捷科技股份有限公司",
 			"position" : "DGS技术经理",
 			"city" : "南京",
-			"description" : "1. 负责敏捷 DM 企业网盘的后台微服务技术架构搭建、核心模块开发；<br>2. 担任 DGS 技术经理，负责敏捷 DGS 安全卫士服务端4.0版本的管理推进、核心模块开发以及代码质量管理工作<br>3. 在职期间，参与搭建了 Agile Platform 微服务平台，并主导完成了 DM 1.0 单体架构到 DM 2.0 微服务架构以及 DGS 1.0 单体架构到 DGS 4.0 微服务架构的升级改造<br>4. 主导完成了 DGS 4.0 各业务模块的优化与重构，带领整个团队拼搏了一年时间使其成功上线<br>5. 在研发部门发起了多场技术培训，主导确立了公司的 Java 代码规范制度"
+			"description" : "1. 担任 DGS 技术经理，负责敏捷 DGS 安全卫士的管理推进、需求规划、核心模块开发工作<br>2. 主导敏捷 DM 企业网盘的后端微服务架构搭建、核心模块开发<br>3. 参与搭建 Agile Platform 微服务平台"
 		},
 		{
 			"form_date" : "2016.03",
@@ -56,21 +56,14 @@ window.resume = {
 			"to_date" : "2021.06",
 			"project_name" : "敏捷科技·DGS数据安全卫士",
 			"position" : "技术经理",
-			"description" : "DGS 是一套数据安全管理一体化平台，采用全方位、全内容、全过程的数据管理与安全保护手段，通过智能安全分析技术、操作系统内核技术、高强度的透明加密技术、灵活易用的安全策略，为企业提供多重数据安全防护服务，有效地解决诸多数据安全隐患。主要模块：<br>1. 基础平台 —— 授权设置，用户管理，域同步管理，流程管理，敏感词管理，全文检索，系统设置，终端管理，审计日志<br>2. 数据安全(DG) —— 文件安全，文件外发，文件授权，打印安全，备份安全<br>3. 文件追溯(DT) —— 屏幕溯源，文件溯源，打印溯源，移动端溯源，文件追踪<br>4. 数据防泄漏(DLP) —— 邮件管控，即时通讯管控，移动外设管控，文件操作管控，网络传输管控<br>5. 安全网关(SXG) —— 应用安全网关(SAG)，加解密网关(SFG)，邮件安全网关(SMG)，数据脱敏网关(SDG)<br><br>主要技术：Spring Boot, Spring Cloud Alibaba, MyBatis, MySQL, Redis, RabbitMQ, Elasticsearch, MinIO<br><br>主要成就：<br>1. 主导完成了 DGS 1.0 单体架构到 DGS 4.0 微服务架构的升级改造<br>2. 主导对 DGS 4.0 的各业务模块进行了优化与重构，确立了 DGS 4.0 产品整体的技术架构<br>3. 确立了 DGS 团队的工作制度、代码规范等，带领整个团队拼搏了一年时间使其成功上线。"
+			"description" : "DGS 是一套数据安全管理一体化平台，采用全方位、全内容、全过程的数据管理与安全保护手段，通过智能安全分析技术、操作系统内核技术、高强度的透明加密技术、灵活易用的安全策略，为企业提供多重数据安全防护服务，有效地解决诸多数据安全隐患。<br><br>主要模块：<br>1. 基础平台 —— 授权设置，用户管理，域同步管理，流程管理，敏感词管理，全文检索，系统设置，终端管理，审计日志<br>2. 数据安全 —— 文件安全，文件外发，文件授权，打印安全，备份安全<br>3. 文件追溯 —— 屏幕溯源，文件溯源，打印溯源，移动端溯源，文件追踪<br>4. 数据防泄漏 —— 邮件管控，即时通讯管控，移动外设管控，文件操作管控，网络传输管控<br>5. 安全网关 —— 应用安全网关，加解密网关，邮件安全网关，数据脱敏网关<br><br>技术栈：<br>Spring Boot, Spring Cloud Alibaba, MyBatis, MySQL, Redis, RabbitMQ, Elasticsearch, MinIO<br><br>个人负责：<br>1. 主导完成了 DGS 1.0 单体架构到 DGS 4.0 微服务架构的升级改造<br>2. 主导对 DGS 4.0 的各业务模块进行了优化与重构，确立了 DGS 4.0 产品整体的技术架构<br>3. 确立了 DGS 团队的工作制度、代码规范等，带领整个团队拼搏了一年时间使其成功上线。"
 		},
 		{
 			"form_date" : "2019.03",
 			"to_date" : "2020.08",
 			"project_name" : "敏捷科技·DM企业网盘",
 			"position" : "Java工程师",
-			"description" : "DM 是一套私有化部署的多人共享协作、文件极速传输、分布式安全存储、文档安全管理解决方案。产品特性：<br>1. 集中管理 —— 提供个人文档中心和企业文档库管理，具备标签、评论等特色功能；<br>2. 协同高效 —— 支持文档内部共享和链接外发，具备在线预览、编辑功能；<br>3. 智能搜索 —— 采用 ElasticSearch 智能搜索引擎，更加智能化、多元化；<br>4. 权限管控 —— 完善的权限管理与安全机制，提供多层次颗粒度的权限控制；<br>5. 外链分享 —— 提供文件分享外链地址，支持设置使用权限和阅读次数等；<br>6. 兼容集成 —— 多浏览器、多系统兼容，支持跨平台部署；<br>7. 海量存储 —— 提供 PB 级存储管理功能，支持分布式部署。<br><br>主要技术：Spring Boot, Spring Cloud Alibaba, MyBatis, MySQL, Redis, RabbitMQ, Elasticsearch, MinIO<br><br>在职期间参与搭建了 Agile Platform 微服务平台，并主导完成了 DM 1.0 单体架构到 DM 2.0 微服务架构的升级改造，使 DM 技术架构趋于稳定形态。"
-		},
-		{
-			"form_date" : "2016.03",
-			"to_date" : "2016.05",
-			"project_name" : "神州数码云安全管理平台",
-			"position" : "Java工程师",
-			"description" : "云安全管理平台用于管理神州数码安全云 Saas 服务平台，便于用户将自己的网站申请接入神州数码云 Web 防火墙用于安全防护，免受黑客威胁。共有三个模块：<br>1. 云防御 —— CDN 加速、防 Ddos、防跨站、防注入、防盗链、防篡改、防挂马、防 CC 攻击等，云 Web 防火墙保护网站免受黑客威胁<br>2. 智能终端管理 —— 移动设备管理、移动应用管理、移动内容管理<br>3. 安全渗透扫描 —— 黑白盒扫描、修复前安全评估报告、定制化的网站安全防护规则、修复后网站安全评估对比<br><br>后端技术：Spring Framework, Spring Data JPA, Shiro, MySQL<br>前端技术：Bootstrap, jQuery, Sea.js<br><br>本人负责该项目云防御模块的后端开发、前端 js 数据绑定以及前端部分 JavaScript 插件的调优。"
+			"description" : "DM 是一套私有化部署的多人共享协作、文件极速传输、分布式安全存储、文档安全管理解决方案。<br><br>产品特性：<br>1. 集中管理 —— 提供个人文档中心和企业文档库管理，具备标签、评论等特色功能；<br>2. 协同高效 —— 支持文档内部共享和链接外发，具备在线预览、编辑功能；<br>3. 智能搜索 —— 采用 ElasticSearch 智能搜索引擎，更加智能化、多元化；<br>4. 权限管控 —— 完善的权限管理与安全机制，提供多层次颗粒度的权限控制；<br>5. 外链分享 —— 提供文件分享外链地址，支持设置使用权限和阅读次数等；<br>6. 兼容集成 —— 多浏览器、多系统兼容，支持跨平台部署；<br>7. 海量存储 —— 提供 PB 级存储管理功能，支持分布式部署。<br><br>技术栈：<br>Spring Boot, Spring Cloud Alibaba, MyBatis, MySQL, Redis, RabbitMQ, Elasticsearch, MinIO<br><br>个人负责：<br>1. 参与搭建了 Agile Platform 微服务平台<br>2. 主导完成了 DM 1.0 单体架构到 DM 2.0 微服务架构的升级改造"
 		}
 	],
 	"professional_skills" : "1. 有多年 Java 后端开发经验，掌握 Java 语言特性与常用设计模式，了解 JVM 调优、并发与多线程等技术<br>2. 熟悉 Web 主流框架的实现原理及设计思想，如 Spring Framework、Spring Boot、Spring Cloud、MyBatis 等<br>3. 熟悉 MySQL 海量数据存储、架构设计及其性能优化，熟悉数据库中间件 ShardingSphere<br>4. 熟悉团队协作 (Git, Maven)、容器与代理 (Tomcat, Nginx)、JVM相关 (JMC, jstack, jmap, jstat, btrace)、Linux系统分析 (vmstat, iostat, ifstat, netstat, dstat)等常用工具集<br>5. 熟悉大型分布式架构、微服务架构的设计与开发，熟悉分布式缓存 (Redis)、分布式消息服务中间件 (RabbitMQ, RocketMQ, Kafka)、分布式搜索引擎 (Elasticsearch, ELK) 等架构的应用",
@@ -84,5 +77,5 @@ window.resume = {
 			"degree" : "U2FsdGVkX1/YYWjxGdCEEIFxIY9gXQ=="
 		}
 	],
-	"self_evaluation" : "一直以来我对软件开发都充满着极大的热情，对互联网领域的技术变化有敏锐的嗅觉。会经常阅读开源技术网站，自我感觉学习能力不错，喜欢钻研新技术，会自己私下开发一些小例子发布到开源技术网站，对自己的代码质量有较高要求，喜欢编写有创造性的、优雅的代码，对项目的技术攻坚、架构设计比较感兴趣，未来我将以技术方向为发展目标努力成为一名优秀的资深高级工程师。"
+	"self_evaluation" : ""
 };
