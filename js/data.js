1f82f1423e56eebdddd4d01058b87d1738d3a899
lihaitao <lihaitao.me@qq.com>
2023-03-29 23:09:19 +0800

    修改简历内容

--- js/data.js
+++ js/data.js
@@ -20,17 +20,17 @@ window.resume = {
 			"form_date" : "2021.09",
 			"to_date" : "至今",
 			"company" : "鼎捷软件股份有限公司江苏分公司",
-			"position" : "Java资深工程师",
+			"position" : "资深Java工程师",
 			"city" : "南京",
-			"description" : "1. 鼎捷 PaaS 平台中心微服务平台开发<br>2. 鼎捷云开发者门户后端开发<br>3. 鼎捷 Athena 开发平台低代码模块后端开发"
+			"description" : "1. 负责鼎捷 PaaS 微服务平台开发<br>2. 负责鼎捷云开发者门户后端开发<br>3. 负责鼎捷 Athena 开发平台低代码模块后端开发"
 		},
 		{
 			"form_date" : "2019.03",
 			"to_date" : "2021.06",
 			"company" : "江苏敏捷科技股份有限公司",
-			"position" : "DGS技术经理",
+			"position" : "技术经理",
 			"city" : "南京",
-			"description" : "1. 担任 DGS 技术经理，负责敏捷 DGS 安全卫士的管理推进、需求规划、核心模块开发工作<br>2. 主导敏捷 DM 企业网盘的后端微服务架构搭建、核心模块开发<br>3. 参与搭建 Agile Platform 微服务平台"
+			"description" : "1. 担任技术经理，负责敏捷 DGS 安全卫士的管理推进、迭代规划、核心模块开发<br>2. 主导敏捷 DM 企业网盘的后端微服务架构搭建、核心模块开发<br>3. 参与搭建 Agile Platform 微服务平台"
 		},
 		{
 			"form_date" : "2016.03",
@@ -38,7 +38,7 @@ window.resume = {
 			"company" : "神州数码（中国）有限公司",
 			"position" : "Java工程师",
 			"city" : "北京",
-			"description" : "负责神州云计算 Marketplace 项目（云市场）的后端开发与前端 js 数据绑定工作"
+			"description" : "负责神州云计算 Marketplace 项目（云市场）的后端开发与前端 js 数据绑定"
 		},
 		{
 			"form_date" : "2015.02",
@@ -46,7 +46,7 @@ window.resume = {
 			"company" : "北京北控伟仕软件工程技术有限公司",
 			"position" : "Java工程师",
 			"city" : "北京",
-			"description" : "负责北京社会保险管理信息系统（五险系统）的前后端开发工作"
+			"description" : "负责北京社会保险管理信息系统（五险系统）的前后端开发"
 		}
 	],
 	"project_exp" :
@@ -56,14 +56,14 @@ window.resume = {
 			"to_date" : "2021.06",
 			"project_name" : "敏捷科技·DGS数据安全卫士",
 			"position" : "技术经理",
-			"description" : "DGS 是一套数据安全管理一体化平台，采用全方位、全内容、全过程的数据管理与安全保护手段，通过智能安全分析技术、操作系统内核技术、高强度的透明加密技术、灵活易用的安全策略，为企业提供多重数据安全防护服务，有效地解决诸多数据安全隐患。<br><br>主要模块：<br>1. 基础平台 —— 授权设置，用户管理，域同步管理，流程管理，敏感词管理，全文检索，系统设置，终端管理，审计日志<br>2. 数据安全 —— 文件安全，文件外发，文件授权，打印安全，备份安全<br>3. 文件追溯 —— 屏幕溯源，文件溯源，打印溯源，移动端溯源，文件追踪<br>4. 数据防泄漏 —— 邮件管控，即时通讯管控，移动外设管控，文件操作管控，网络传输管控<br>5. 安全网关 —— 应用安全网关，加解密网关，邮件安全网关，数据脱敏网关<br><br>技术栈：<br>Spring Boot, Spring Cloud Alibaba, MyBatis, MySQL, Redis, RabbitMQ, Elasticsearch, MinIO<br><br>个人负责：<br>1. 主导完成了 DGS 1.0 单体架构到 DGS 4.0 微服务架构的升级改造<br>2. 主导对 DGS 4.0 的各业务模块进行了优化与重构，确立了 DGS 4.0 产品整体的技术架构<br>3. 确立了 DGS 团队的工作制度、代码规范等，带领整个团队拼搏了一年时间使其成功上线。"
+			"description" : "DGS 是一套数据安全管理一体化平台，采用全方位、全内容、全过程的数据管理与安全保护手段，通过智能安全分析技术、操作系统内核技术、高强度的透明加密技术、灵活易用的安全策略，为企业提供多重数据安全防护服务，有效地解决诸多数据安全隐患。<br><br>主要模块：<br>1. 基础平台 —— 授权设置，用户管理，域同步管理，流程管理，敏感词管理，全文检索，系统设置，终端管理，审计日志<br>2. 数据安全 —— 文件安全，文件外发，文件授权，打印安全，备份安全<br>3. 文件追溯 —— 屏幕溯源，文件溯源，打印溯源，移动端溯源，文件追踪<br>4. 数据防泄漏 —— 邮件管控，即时通讯管控，移动外设管控，文件操作管控，网络传输管控<br>5. 安全网关 —— 应用安全网关，加解密网关，邮件安全网关，数据脱敏网关<br><br>技术栈：<br>Spring Boot, Spring Cloud Alibaba, MyBatis, MySQL, Redis, RabbitMQ, Elasticsearch, MinIO<br><br>工作业绩：<br>1. 主导完成了 DGS 1.0 单体架构到 DGS 4.0 微服务架构的升级改造<br>2. 主导对 DGS 4.0 的各业务模块进行了优化与重构，确立了 DGS 4.0 产品整体的技术架构<br>3. 确立了 DGS 团队的工作流程、代码规范等，带领整个团队拼搏了一年时间使其成功上线"
 		},
 		{
 			"form_date" : "2019.03",
 			"to_date" : "2020.08",
 			"project_name" : "敏捷科技·DM企业网盘",
 			"position" : "Java工程师",
-			"description" : "DM 是一套私有化部署的多人共享协作、文件极速传输、分布式安全存储、文档安全管理解决方案。<br><br>产品特性：<br>1. 集中管理 —— 提供个人文档中心和企业文档库管理，具备标签、评论等特色功能；<br>2. 协同高效 —— 支持文档内部共享和链接外发，具备在线预览、编辑功能；<br>3. 智能搜索 —— 采用 ElasticSearch 智能搜索引擎，更加智能化、多元化；<br>4. 权限管控 —— 完善的权限管理与安全机制，提供多层次颗粒度的权限控制；<br>5. 外链分享 —— 提供文件分享外链地址，支持设置使用权限和阅读次数等；<br>6. 兼容集成 —— 多浏览器、多系统兼容，支持跨平台部署；<br>7. 海量存储 —— 提供 PB 级存储管理功能，支持分布式部署。<br><br>技术栈：<br>Spring Boot, Spring Cloud Alibaba, MyBatis, MySQL, Redis, RabbitMQ, Elasticsearch, MinIO<br><br>个人负责：<br>1. 参与搭建了 Agile Platform 微服务平台<br>2. 主导完成了 DM 1.0 单体架构到 DM 2.0 微服务架构的升级改造"
+			"description" : "DM 是一套私有化部署的多人共享协作、文件极速传输、分布式安全存储、文档安全管理解决方案。<br><br>产品特性：<br>1. 集中管理 —— 提供个人文档中心和企业文档库管理，具备标签、评论等特色功能；<br>2. 协同高效 —— 支持文档内部共享和链接外发，具备在线预览、编辑功能；<br>3. 智能搜索 —— 采用 ElasticSearch 智能搜索引擎，更加智能化、多元化；<br>4. 权限管控 —— 完善的权限管理与安全机制，提供多层次颗粒度的权限控制；<br>5. 外链分享 —— 提供文件分享外链地址，支持设置使用权限和阅读次数等；<br>6. 兼容集成 —— 多浏览器、多系统兼容，支持跨平台部署；<br>7. 海量存储 —— 提供 PB 级存储管理功能，支持分布式部署。<br><br>技术栈：<br>Spring Boot, Spring Cloud Alibaba, MyBatis, MySQL, Redis, RabbitMQ, Elasticsearch, MinIO<br><br>工作业绩：<br>1. 参与搭建了 Agile Platform 微服务平台<br>2. 主导搭建了DM企业网盘后端的微服务架构，并参与了核心业务模块的开发工作"
 		}
 	],
 	"professional_skills" : "1. 有多年 Java 后端开发经验，掌握 Java 语言特性与常用设计模式，了解 JVM 调优、并发与多线程等技术<br>2. 熟悉 Web 主流框架的实现原理及设计思想，如 Spring Framework、Spring Boot、Spring Cloud、MyBatis 等<br>3. 熟悉 MySQL 海量数据存储、架构设计及其性能优化，熟悉数据库中间件 ShardingSphere<br>4. 熟悉团队协作 (Git, Maven)、容器与代理 (Tomcat, Nginx)、JVM相关 (JMC, jstack, jmap, jstat, btrace)、Linux系统分析 (vmstat, iostat, ifstat, netstat, dstat)等常用工具集<br>5. 熟悉大型分布式架构、微服务架构的设计与开发，熟悉分布式缓存 (Redis)、分布式消息服务中间件 (RabbitMQ, RocketMQ, Kafka)、分布式搜索引擎 (Elasticsearch, ELK) 等架构的应用",
@@ -74,7 +74,14 @@ window.resume = {
 			"to_date" : "U2FsdGVkX1+6AFMgScHkjbdwStnZin8=",
 			"school" : "U2FsdGVkX1/AU+G7TIFmQ9LgkcK01zoSAELHE6g8laVnKA==",
 			"major" : "U2FsdGVkX19u5JnEuvjPMUJkPCCunEHz4kz9Hv0hux213Oc5Cw==",
-			"degree" : "U2FsdGVkX1/YYWjxGdCEEIFxIY9gXQ=="
+			"degree" : "U2FsdGVkX190i0mpnrFqOKLYxn92EBnPj29f6n7acLuk6g=="
+		},
+		{
+			"form_date" : "U2FsdGVkX1+qMR0+DRJEm7n0PGgwlwg=",
+			"to_date" : "U2FsdGVkX19TZa7dhd+norjyusymCpk=",
+			"school" : "U2FsdGVkX1/AU+G7TIFmQ9LgkcK01zoSAELHE6g8laVnKA==",
+			"major" : "U2FsdGVkX1+yZNLJXfNi1JxBRcq4jMHc86BwKQ/PAjSXsTsg5g==",
+			"degree" : "U2FsdGVkX19WWidz9jHx+ykfxJ3TKwYrl4oPEOY9otNW1Q=="
 		}
 	],
 	"self_evaluation" : ""
